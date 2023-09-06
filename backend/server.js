054d1b4ecd76934f5956c0330652c5c8c7ffb6b1
Disha <bidisha2125@gmail.com>
2023-09-06 17:47:21 +0530

    Added the whole registration feature for User

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const chats = require("./data/data")
+const colors = require('colors');
+const chats = require("./data/data");
+const connectDB = require('./config/db');
+const userRouts = require("./routes/userRoutes");
+const { registerUser } = require('./controllers/userControllers');
+
 
 const app = express();
 dotenv.config();
+connectDB();
+
+app.use(express.json());
 
 app.get('/', (req,res) => {
     res.send("Api is running")
 });
 
-app.get('/api/chat', (req,res) => {
-    res.send(chats)
-})
+app.use('/api/user', registerUser);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`listening on port ${PORT}`));
+app.listen(PORT, console.log(`listening on port ${PORT}`.blue.bold));
