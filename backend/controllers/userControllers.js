1594ff4ea8b43147e31153ba6be77b7a50f67ec7
Disha <bidisha2125@gmail.com>
2023-09-06 17:46:05 +0530

    Created controller for registration

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,47 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const generateToken = require('../config/generateToken');
+
+const registerUser = asyncHandler( async(req, res) => {
+    const { name, email, password, pic } = req.body;
+
+    //Checking if any of these fields are epty or not
+    if(!name || !email || !password){
+        res.status(400);
+        throw new Error("Please enter all the fields");
+    }
+
+    //finding the unique email-id if it is present in the database already or not
+    const userExists = await User.findOne({ email });
+
+    //if presents throw the error 400 bad request
+    if(userExists) {
+        res.status(400);
+        throw new Error("user Already exists");
+    }
+
+    //if the email not present in the database create the new user in database
+    const user = await User.create({
+        name,
+        email,
+        password,
+        pic,
+    });
+
+     if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            pic: user.pic,
+            token: generateToken(user._id)
+        })
+        console.log("User created successfully")
+     } else {
+        res.status(400);
+        throw new Error("Failed to create User");
+     }
+
+});
+
+module.exports = { registerUser };
