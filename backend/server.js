b547c66913f05634606d148639cf36e0f46a09cb
Disha <bidisha2125@gmail.com>
2023-09-10 22:39:36 +0530

    Added deployment code for production

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,25 @@ app.use('/api/user', userRouts);
 app.use('/api/chat', chatRoutes);
 app.use("/api/message", messageRoutes);
 
+//-------------------DEPLOYMENT----------------
+
+// const __dirname1 = path.resolve();
+
+// if(process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname1, "/frontend/build")))
+
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+//     })
+
+// } else {
+//     app.get('/', (req, res) => {
+//         res.send("Api is running")
+//     });
+// }
+
+//-------------------DEPLOYMENT----------------
+
 // Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
